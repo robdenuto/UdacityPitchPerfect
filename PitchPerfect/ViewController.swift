6fe19ebc0ba8ae4c644fceb8444edd47bfc586e8
rdenuto <rdenuto@audible.com>
2016-11-22 22:43:21 -0500

    Delegation & Recording - Renaming the Recording View Controller

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled=false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called!")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        recordingLabel.text="Recording in Progress"
+        recordButton.isEnabled=false
+        stopRecordingButton.isEnabled=true
+        
+        
+    }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        stopRecordingButton.isEnabled=false
+        recordButton.isEnabled=true
+        recordingLabel.text="Tap to Record"
+    }
 }
 
